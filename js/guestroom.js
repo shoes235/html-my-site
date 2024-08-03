616345efebbfdd60f7ebee3b8a0bd17eea58941b
shoes235 <142568864+shoes235@users.noreply.github.com>
2024-08-04 06:01:04 +0900

    Add files via upload

--- js/guestroom.js
+++ js/guestroom.js
@@ -4,21 +4,28 @@ var month = now.getMonth() + 1; // 0부터 시작하므로 +1
 var day = now.getDate();
 var hour = now.getHours();
 var minute = now.getMinutes();
-var second = now.getSeconds();
+minute = minute < 10 ? "0" + minute : minute;
+second = second < 10 ? "0" + second : second; 
 
 function add_comment(){
-    var time = `${year}년${month}월${day}일 ${hour}시 ${minute}분에 작성`
+    var time_list = document.getElementById("time_list");
+
     var comment = document.getElementById("comment").value;
     var comment_list = document.getElementById("comment_list");
     var new_comment = document.createElement("new_comment");
 
-    new_comment.innerHTML = time + comment;
+    new_comment.innerHTML = comment;
     comment_list.innerHTML ="";
     comment_list.appendChild(new_comment);
     document.getElementById("comment").value = "";
+
+    time_list.innerHTML = "";
+    var time = `${year}-${month}-${day} ${hour}:${minute} 작성`;
+    time_list.innerHTML = time;
 }
 function clear_comment(){
     var comment_list = document.getElementById("comment_list");
+    var time_list = document.getElementById("time_list");
     comment_list.innerHTML = "";
-    time = "";
+    time_list.innerHTML = "";
 }
